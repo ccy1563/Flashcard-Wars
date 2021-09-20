648af2c2acbef2ca8bdc101c8011c06382bd4d83
ccy1563 <ccy1563@gmail.com>
2021-09-20 17:16:54 -0400

    Backend complete, frontend setup

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Flashcard Wars</h1>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
